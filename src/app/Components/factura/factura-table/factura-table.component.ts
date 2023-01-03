ad580804146eef0a46f612b79ad4dc886dc9bac8
sebasgaspar <32582404+sebasgaspar@users.noreply.github.com>
2023-01-03 12:56:36 -0500

    Fix problems with year

--- src/app/Components/factura/factura-table/factura-table.component.ts
+++ src/app/Components/factura/factura-table/factura-table.component.ts
@@ -225,7 +225,6 @@ export class FacturaTableComponent implements OnInit {
     this.tarjeta = 0;
     var mes = new Date();
     mes.setHours(0, 0, 0, 0)
-    mes.setMonth(mes.getMonth() - 1);
     let items = this.allFacturas.filter(item => item.fecha.getMonth() == this.seleccionados 
         && item.fecha.getFullYear() == mes.getFullYear());
     this.facturas = items;
